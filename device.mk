a847cdfec6e45f6d73960e9379f7c85c67dc7ee0
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    whyred: Add powerhint.json for sdm636
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- device.mk
+++ device.mk
@@ -367,6 +367,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Protobuf
