59f032e8aa74ab37cc1124dff8f882a97f92d97c
sbwml <984419930@qq.com>
2020-12-10 13:49:03 +0800

    whyred: bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.confi@2.0.so
    
    Change-Id: I95498c564d7bfff3d877007dec0900354780321d

--- device.mk
+++ device.mk
@@ -193,7 +193,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
