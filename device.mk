db807222e3d0db43898df25dabc7d069a17ef3a7
orges <me@orgesified.ml>
2021-10-13 16:39:40 +0000

    whyred: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: Ieb69a8c8098b7ac236338b3619d2c6cf963b82fc

--- device.mk
+++ device.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # DPM
 PRODUCT_PACKAGES += \
     libshim_dpmframework \
