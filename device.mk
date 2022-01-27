a8a5ed71b132976bdda1a3cc01ddfce94ba3a15d
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    whyred: Drop support for Snapdragon Camera
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- device.mk
+++ device.mk
@@ -134,9 +134,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.6:64 \
