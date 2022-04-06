7cc08be26cd647625412978ad64526e6fb7348cf
Adlai Holler <adlai@google.com>
2022-04-06 10:00:41 +0200

    whyred: props: Enable skia reduceOpsTaskSplitting
    Bug: 183612348 | AOSP
    Change-Id: I8681323390d32a269e53d82e5b25b4efba5f9865 | AOSP

--- properties.mk
+++ properties.mk
@@ -149,7 +149,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    debug.renderengine.backend=threaded
+    debug.renderengine.backend=threaded \
+    renderthread.skia.reduceopstasksplitting=true
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
